41e8d90df96bae69a196d3903349950f1fc6687b
Sergey Petykhov <sergey_petykhov@mail.ru>
2023-05-26 09:08:43 +0300

    change ALLOWED_CORS

--- backend/constants/constants.js
+++ backend/constants/constants.js
@@ -9,13 +9,21 @@ const INTERNAL_SERVER_ERROR_CODE = 500;
 const URL_REGULAR_EXP = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)?$/im;
 
 const ALLOWED_CORS = [
-  'http://mesto-psv.nomoredomains.monster',
-  'https://mesto-psv.nomoredomains.monster',
-  'http://api.mesto-psv.nomoredomains.monster',
-  'https://api.mesto-psv.nomoredomains.monster',
-  'http://158.160.15.128',
-  'https://158.160.15.128',
-  'http://localhost:3000',
+  '//mesto-psv.nomoredomains.monster',
+  '//mesto-psv.nomoredomains.monster',
+  '//api.mesto-psv.nomoredomains.monster',
+  '//api.mesto-psv.nomoredomains.monster',
+  '//158.160.15.128',
+  '//158.160.15.128',
+  '//localhost:3000',
+
+  // 'http://mesto-psv.nomoredomains.monster',
+  // 'https://mesto-psv.nomoredomains.monster',
+  // 'http://api.mesto-psv.nomoredomains.monster',
+  // 'https://api.mesto-psv.nomoredomains.monster',
+  // 'http://158.160.15.128',
+  // 'https://158.160.15.128',
+  // 'http://localhost:3000',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
